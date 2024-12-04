504c9f926751b70baf77b3daceaeca0fe19b1a27
Mohsen Asfia <mohsen.asfia@maersk.com>
2024-12-04 16:50:01 +0100

    use env varialbes instead of the hard-coded ones

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -1,10 +1,9 @@
 import { createClient, SanityClient } from '@sanity/client'
-
 export const sanityClient: SanityClient = createClient({
-  projectId: '<token>',
-  dataset: 'production',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   useCdn: true,
-  apiVersion: 'api',
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
 })
 
 export default sanityClient
