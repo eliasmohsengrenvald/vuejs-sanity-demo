19453a347e848b6a0386ae12021877c9731de91b
Mohsen Asfia <mohsen.asfia@maersk.com>
2024-12-04 21:53:00 +0100

    get the env variables working with vercel

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -1,17 +1,13 @@
 import { createClient, SanityClient } from '@sanity/client'
 
-console.log('sss', process.env.VITE_SANITY_PROJECT_ID)
-
 export const sanityClient: SanityClient = createClient({
   projectId: import.meta.env.PROD
-    ? process.env.VITE_SANITY_PROJECT_ID
+    ? process.env.SANITY_PROJECT_ID
     : import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PROD
-    ? process.env.VITE_SANITY_DATASET
-    : import.meta.env.VITE_SANITY_DATASET,
+  dataset: import.meta.env.PROD ? process.env.SANITY_DATASET : import.meta.env.VITE_SANITY_DATASET,
   useCdn: true,
   apiVersion: import.meta.env.PROD
-    ? process.env.VITE_SANITY_API_VERSION
+    ? process.env.SANITY_API_VERSION
     : import.meta.env.VITE_SANITY_API_VERSION,
 })
 
