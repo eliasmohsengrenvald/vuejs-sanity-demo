b31c82d77101ec0aee963b721a53ed56af0b90ef
Mohsen Asfia <mohsen.asfia@maersk.com>
2024-12-04 21:57:36 +0100

    add the latest posts in the middle

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -1,14 +1,10 @@
 import { createClient, SanityClient } from '@sanity/client'
 
 export const sanityClient: SanityClient = createClient({
-  projectId: import.meta.env.PROD
-    ? process.env.SANITY_PROJECT_ID
-    : import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PROD ? process.env.SANITY_DATASET : import.meta.env.VITE_SANITY_DATASET,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   useCdn: true,
-  apiVersion: import.meta.env.PROD
-    ? process.env.SANITY_API_VERSION
-    : import.meta.env.VITE_SANITY_API_VERSION,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
 })
 
 export default sanityClient
