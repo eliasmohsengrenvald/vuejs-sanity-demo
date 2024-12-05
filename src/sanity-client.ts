ee59e6b48b7bb2397746198a1a5dcce2b7597616
Mohsen Asfia <mohsen.asfia@maersk.com>
2024-12-05 08:42:19 +0100

    try the former env approach for vercel

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -1,14 +1,10 @@
 import { createClient, SanityClient } from '@sanity/client'
 
-const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || process.env.VITE_SANITY_PROJECT_ID
-const dataset = import.meta.env.VITE_SANITY_DATASET || process.env.VITE_SANITY_DATASET
-const apiVersion = import.meta.env.VITE_SANITY_API_VERSION || process.env.VITE_SANITY_API_VERSION
-
 export const sanityClient: SanityClient = createClient({
-  projectId,
-  dataset,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   useCdn: true,
-  apiVersion,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
 })
 
 export default sanityClient
