53befc3636aa521769006f96a04d305c499dcaa7
Mohsen Asfia <mohsen.asfia@maersk.com>
2024-12-05 08:39:34 +0100

    try to fix the issue that the posts not getting through in vercel

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -1,10 +1,14 @@
 import { createClient, SanityClient } from '@sanity/client'
 
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || process.env.VITE_SANITY_PROJECT_ID
+const dataset = import.meta.env.VITE_SANITY_DATASET || process.env.VITE_SANITY_DATASET
+const apiVersion = import.meta.env.VITE_SANITY_API_VERSION || process.env.VITE_SANITY_API_VERSION
+
 export const sanityClient: SanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET,
+  projectId,
+  dataset,
   useCdn: true,
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+  apiVersion,
 })
 
 export default sanityClient
